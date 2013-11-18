2d6fd1cdca3f0945b568d397ce5b6ec7504519f5
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-11-18 16:04:16 +0600

    Remove outdated code

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,9 +5,4 @@ class Message < ActiveRecord::Base
   validates_presence_of :text
 
   scope :dialog, -> (first_user, second_user) { where('(sender_id = :sender_id AND recipient_id = :recipient_id) OR (sender_id = :recipient_id AND recipient_id = :sender_id)', sender_id: first_user.id, recipient_id: second_user.id) }
-
-  def read?
-    read_at.present?
-  end
-
 end
